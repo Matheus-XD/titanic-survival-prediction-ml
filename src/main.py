fa494074581e73761bb4f28383ad7053fe701bee
Matheus de Sousa Soares <163562263+Matheus-XD@users.noreply.github.com>
2026-05-01 16:30:44 -0300

    Remove explanatory comments from main.py
    Removed comments explaining the code logic in main.py.

--- src/main.py
+++ src/main.py
@@ -42,14 +42,3 @@ print("Acurácia Logistic Regretion: ", modelLR.score(X_test, y_test))
 print("Acurácia Decision Tree: ", modelDT.score(X_test, y_test))
 print("Acurácia Randon Forest: ", modelRF.score(X_test, y_test))
 
-''' O que eu entendi até agora:
-1 - foi criado uma variavel chamada df (dataframe) e ela vai receber um tipo de planilha com os dados lidos do arquivo train.csv lidos pela função pd.read_csv 
-2 - a segunda linha que filtra apenas o que é necessario para a análise.
-3 - Transformar conteudo da coluna sexo em numero, porque o modelo funciona melhor com numeros do que com nomes.
-4 - Remove linhas que tem dados faltando dados faltantes para não gerar erro no codigo
-5 - separa entrada e saída, x vai receber os dados para analise e y vai retornar a resposta
-6 - divisão treino e teste, cria 4 variaveis (x e y para treino e x e y para  teste) para receber os 4 valores retornados pela função train_test_split() que recebe os valores de x e y
-agora o que significa a linha model = LogisticRegression()
-7 - cria o modelo de LogisticRegression() numa variavel chamada model 
-8 - a função model.fit() recebe os valores de x train e y train para treinar o modelo
-agora para finalizar o print(model.score(X_test e Y_test)) servem pra mostrar o quão esse modelo é preciso né? como essa função funciona? '''
